615c76697b1d1160a5120ab49000ee8bb0a450c6
Pratty <tpratishtha12@gmail.com>
2026-09-26 18:38:01 +0530

    Delete crm-root directory

--- crm-root/build_files.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-echo "Installing Python dependencies..."
-python3 -m pip install -r requirements.txt
-
-echo "Collecting static files..."
-cd Django-CRM-main/src
-python3 manage.py collectstatic --noinput --clear
